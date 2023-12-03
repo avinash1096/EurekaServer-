baf1ae472694cb39b1988f567fd8055a27d5840c
avinash1096 <avinash.gupta1096@gmail.com>
2023-12-03 16:35:21 +0530

    First commit

---
+++ src/test/java/com/eurekaServer/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
